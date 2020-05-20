7e86c25fd23bde2d7c8fa036d10ab9aec1212990
Ivan <ikvuch@gmail.com>
2020-05-20 20:52:06 +0300

    Initial commit

---
+++ app/src/main/java/com/ikvych/cocktail/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ikvych.cocktail.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ikvych.cocktail.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

42afdaa9e198b7efbd1f9e660c70daa7da9ce046
Ivan <ikvuch@gmail.com>
2020-06-24 21:42:03 +0300

    Added column created at Drink Entity

--- app/src/main/java/com/ikvych/cocktail/data/database/DrinkDao.kt
+++ app/src/main/java/com/ikvych/cocktail/data/database/DrinkDao.kt
@@ -13,9 +13,9 @@ interface DrinkDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(drink: Drink)
 
-    @Query("SELECT * FROM drink")
+    @Query("SELECT * FROM drink ORDER BY created DESC")
     fun getAllDrinks() : LiveData<List<Drink>>
 
-    @Query("SELECT * FROM drink WHERE if_favorite")
+    @Query("SELECT * FROM drink WHERE if_favorite ORDER BY created DESC")
     fun getAllFavoriteDrinks() : LiveData<List<Drink>>
 }
--- app/src/main/java/com/ikvych/cocktail/data/database/DrinkDataBase.kt
+++ app/src/main/java/com/ikvych/cocktail/data/database/DrinkDataBase.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.ikvych.cocktail.convertor.db.DateConverter
 import com.ikvych.cocktail.data.entity.Drink
 
 
-@Database(entities = [Drink::class], version = 3, exportSchema = false)
-
+@Database(entities = [Drink::class], version = 5, exportSchema = false)
+@TypeConverters(DateConverter::class)
 abstract class DrinkDataBase : RoomDatabase() {
 
     abstract fun drinkDao(): DrinkDao

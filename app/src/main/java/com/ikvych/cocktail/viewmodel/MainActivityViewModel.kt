5114f07c954ff10904c74fb1b53e5b5d1b60f872
Ivan <ikvuch@gmail.com>
2020-07-03 16:12:51 +0300

    Added comments to method getDrinkOfTheDay

--- app/src/main/java/com/ikvych/cocktail/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/ikvych/cocktail/viewmodel/MainActivityViewModel.kt
@@ -24,21 +24,33 @@ class MainActivityViewModel(
     }
 
     fun getDrinkOfTheDay(): Drink? {
+        //Оскільки ми викликаємо цей метод з MainActivity, яка по своїй природні працює тільки з базою даних, то
+        //даний метод не працює при повному перевстановленні додатку, оскільки разом з додатком видаляється сама база даних
+        //в якій ми шукаємо напій дня
+
+        //отримаю і форматую сьогоднішню дату, яка являється унікальним ключем для напою дня
         val currentDate = Date()
         val pattern = "MM-dd-yyyy"
         val simpleDateFormat = SimpleDateFormat(pattern, Locale.getDefault())
         val stringDate: String = simpleDateFormat.format(currentDate)
 
+        //шукаю в базі даних напій який має сьогоднішню дату в якості ключа
         var drinkOfTheDay = drinkRepository.findDrinkOfTheDay(stringDate)
+        //якщо напій не знайдено значить сьогодні напій дня ще не визначався і потрібно його обрати
         if (drinkOfTheDay == null) {
+            //витягаю з БД усі напої, якщо їх немає, або список порожній, значить
+            // напій немає з чого вибирати і тому виходимо з методу getDrinkOfTheDay()
             val allDrinks = drinkRepository.getAllDrinksFromDb()
             if (allDrinks.isNullOrEmpty()) {
                 return null
             } else {
+                //якщо список не порожній, дістаємо з нього випадковий напій і присвоюємо йому сьогоднішню дату
+                // в якості унікального ключа, і зберігаємо в базу даних
                 val newDrinkOfTheDay: Drink = allDrinks.random()
                 newDrinkOfTheDay.setDrinkOfDay(stringDate)
                 drinkRepository.saveDrinkIntoDb(newDrinkOfTheDay)
             }
+            //знову шукаємо напі по даній даті(очікується той що тільки що зберегли)
             drinkOfTheDay = drinkRepository.findDrinkOfTheDay(stringDate)
         }
         return drinkOfTheDay

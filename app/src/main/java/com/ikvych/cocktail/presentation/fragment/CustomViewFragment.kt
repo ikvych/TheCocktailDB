366cfa4133d09cb521469aa7f26e6e5bff6a518d
ikvych <ikvuch@gmail.com>
2020-08-26 17:04:42 +0300

    Created ViewGroup with max child height

--- app/src/main/java/com/ikvych/cocktail/presentation/fragment/CustomViewFragment.kt
+++ app/src/main/java/com/ikvych/cocktail/presentation/fragment/CustomViewFragment.kt
@@ -1,6 +1,7 @@
 package com.ikvych.cocktail.presentation.fragment
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,7 @@ import com.ikvych.cocktail.R
 import com.ikvych.cocktail.databinding.FragmentCustomViewBinding
 import com.ikvych.cocktail.presentation.fragment.base.BaseFragment
 import com.ikvych.cocktail.viewmodel.base.BaseViewModel
+import kotlinx.android.synthetic.main.fragment_custom_view.*
 import kotlin.reflect.KClass
 
 class CustomViewFragment : BaseFragment<BaseViewModel, FragmentCustomViewBinding>() {
@@ -18,9 +20,15 @@ class CustomViewFragment : BaseFragment<BaseViewModel, FragmentCustomViewBinding
     override val viewModelClass: KClass<BaseViewModel>
         get() = BaseViewModel::class
 
+    override fun configureView(view: View, savedInstanceState: Bundle?) {
+        val text = "Text\n2nd line\nSample Text"
+        artv_test.maxLines = 2
+        artv_test.ellipsize = TextUtils.TruncateAt.END
+        artv_test.text = text
+    }
+
     companion object {
         @JvmStatic
         fun newInstance() = CustomViewFragment()
-
     }
 }
